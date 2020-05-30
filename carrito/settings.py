3b9d0428743c29e2a312f51c9a907006e4f1f5e2
unknown <paolo.minaya.c@gmail.com>
2020-05-30 16:47:53 -0500

    lo ultimo

--- carrito/settings.py
+++ carrito/settings.py
@@ -58,11 +58,11 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = [
 
-    "https://carritosgo.herokuapp.com/apis_categorias/List_Categorias_Productos/",
-    "https://carritosgo.herokuapp.com/apis_productos/Detail_Productos/",
-    "https://carritosgo.herokuapp.com/apis_productos/List_Productos/",
-    "https://carritosgo.herokuapp.com/apis_user/registrarusuario/",
-    "https://carritosgo.herokuapp.com/apis_user/autenticar/",
+    #"https://carritosgo.herokuapp.com/apis_categorias/List_Categorias_Productos/",
+    #"https://carritosgo.herokuapp.com/apis_productos/Detail_Productos/",
+    #"https://carritosgo.herokuapp.com/apis_productos/List_Productos/",
+    #"https://carritosgo.herokuapp.com/apis_user/registrarusuario/",
+    #"https://carritosgo.herokuapp.com/apis_user/autenticar/",
     "https://sub.example.com",
     "http://localhost:3000",
     "http://127.0.0.1:5500",
